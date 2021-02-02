72a6054b36a278b7ea840026ea2a7e091611fc15
Jcom619 <joshuacompton@my.smccd.edu>
2021-02-02 08:38:31 -0800

    getting close
    still needs to replace previous quote not just add to it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("👀")
+function kQuote() {
+    fetch('https://api.kanye.rest/')
+    .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            let kanTent = document.createElement("h2")
+                kanTent.innerText = json.quote;
+                document.body.appendChild(kanTent)
+        });
+}
+document.getElementById('kWords').addEventListener('click', function() {
+    kQuote()
+});
+
